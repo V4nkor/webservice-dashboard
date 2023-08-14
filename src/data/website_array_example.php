4429e7c1f236567332fc924abdd9dfff94ec6be0
Mathieu Morgat <vanknpix@gmail.com>
2023-08-14 14:44:57 +0200

    Fixed - website_array_example.php

--- src/data/website_array_example.php
+++ src/data/website_array_example.php
@@ -2,8 +2,8 @@
 
 $services = [
     ["portfolio","Portfolio","Projet portfolio","192.168.1.90","www.example.com","",""],
-    ["ftp","FTP server","192.168.1.225","ftp.example.com","21","21"]
-    ["nas","NAS server web interface","192.168.1.50",]
+    ["ftp","FTP server","An FTP server for file storing","192.168.1.225","ftp.example.com","21","21"],
+    ["nas","NAS server web interface","An Openmediavault web interface","192.168.1.50","nas.example.com","7070","7070"]
 ]; 
 
 ?>
